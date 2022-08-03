3372456e644ea20e46e927e21e52da48d56b130e
Sajithsaji007 <saji0151@gmail.com>
2022-08-03 16:50:31 +0530

    Adding all the maven project

---
+++ src/main/java/Maven_Firsst_hweiufwiue/Maven_Firsst_hweiufwiue/App.java
@@ -0,0 +1,13 @@
+package Maven_Firsst_hweiufwiue.Maven_Firsst_hweiufwiue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
